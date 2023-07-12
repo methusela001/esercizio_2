fb96d7bb1236002d2578006bd9bac050bcc95add
Methusela <nethusela777@gmail.com>
2023-07-12 17:04:25 +0200

    1_esercizio

---
+++ src/PrintYourName.java
@@ -0,0 +1,13 @@
+public class PrintYourName {
+    public static void main(String[] args) {
+        System.out.println("Methusela");
+    }
+}
+    /*create a class called PrintYourName
+        provide the class PrintYourName with a main method:
+public static void main(String[] args) { ... }
+        inside the main method write the code that prints your name.
+        Note: in the console, after your name, you will notice a message like the following:
+
+        Process finished with exit code 0
+        Don't worry, it means that you didn't break Java, so there were 0 errors :) */
